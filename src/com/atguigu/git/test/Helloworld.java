c48ef04c3644c16820ef414bd5d0ffebc7d2bee9
shc1314 <sh_c1314@sina.com>
2016-07-08 21:27:19 +0800

    git 01

---
+++ src/com/atguigu/git/test/Helloworld.java
@@ -0,0 +1,14 @@
+package com.atguigu.git.test;
+/** 
+ * @author  作者 : Shuhui Cao
+ * @date 创建时间：2016年7月8日 下午9:26:30 
+ * @version 1.0 
+ * @parameter  
+ * @since  
+ * @return  
+ */
+public class Helloworld {
+	public static void main(String[] args) {
+		System.out.println("hello git");
+	}
+}
